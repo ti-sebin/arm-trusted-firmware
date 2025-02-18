52521b52784acc95a15cdbe87239776b0694e818
Sebin Francis <sebin.francis@ti.com>
2025-02-18 15:19:26 +0530

    PENDING: feat(ti): lpm: Add support for RTC + DDR resume
    Device needs to go through a differnt boot sequence during RTC + DDR
    low power mode resume. RTC only + DDR resume can be detected by checking
    the OFF_MODE magic word in WKUP CTRL MMR. During resume
    
    1. Device needs to send CORE_RESUME message to TIFS compared to sending
    done message to SEC ROM.
    
    2. DRR needs to go through few extra sequence in RTC only + DDR low
    power mode to initialize the DDR without losing it context. Update
    the DDR driver to do the extra sequence when device is resuming
    from RTC only + DDR low power mode.
    
    Change-Id: I85a56c0a5d4722852b635e302cab73b726717965
    Signed-off-by: Sebin Francis <sebin.francis@ti.com>

--- drivers/ti/ti_sci/ti_sci_protocol.h
+++ drivers/ti/ti_sci/ti_sci_protocol.h
@@ -845,4 +845,23 @@ struct tisci_msg_boot_notification_msg {
 	uint32_t extboot_status;
 } __packed;
 
+/**
+ * \brief Request for TISCI_MSG_MIN_CONTEXT_RESTORE.
+ *
+ * \param hdr TISCI header to provide ACK/NAK flags to the host.
+ * \param ctx_lo Low 32-bits of physical pointer to address to use for context restore.
+ * \param ctx_hi High 32-bits of physical pointer to address to use for context restore.
+ *
+ * This message is sent from bootloader to TIFS to indicate that DDR is active and
+ * TIFS can restore the minimal context from the address provided in the ctx_lo and
+ * ctx_hi parameters. This response assumes DDR has been fully restored by bootloader
+ * before it is sent.
+ *
+ */
+struct tisci_msg_min_context_restore_req {
+	struct ti_sci_msg_hdr	hdr;
+	uint32_t			ctx_lo;
+	uint32_t			ctx_hi;
+} __packed;
+
 #endif /* TI_SCI_PROTOCOL_H */
